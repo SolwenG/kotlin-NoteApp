a19edc62fff395d67a94f94619f3ecac85c57795
SolwenG <solene.gouin@orange.fr>
2024-05-14 10:58:54 +0200

    created Note model

---
+++ app/src/main/java/com/example/noteapp/Note.kt
@@ -0,0 +1,36 @@
+package com.example.noteapp
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Note(
+    var title: String? = "",
+    var text: String? = "",
+    var filename: String? = "") : Parcelable {
+
+        constructor(parcel: Parcel): this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readString()
+        ) {}
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(text)
+        parcel.writeString(filename)
+    }
+
+    companion object CREATOR : Parcelable.Creator<Note> {
+        override fun createFromParcel(parcel: Parcel): Note {
+            return Note(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Note?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
